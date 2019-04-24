361adc5166601a419ab32306de46b30cd71d09b5
GaoLei <gaolei@gaoleideMacBook-Pro-2.local>
2019-04-24 18:39:24 +0800

    完美结局

--- Modularization/base/src/main/java/com/example/base/BaseApplication.java
+++ Modularization/base/src/main/java/com/example/base/BaseApplication.java
@@ -15,13 +15,13 @@ public class BaseApplication extends Application {
 
     private void initRouter(BaseApplication myApplication) {
         // 这两行必须写在init之前，否则这些配置在init过程中将无效
-        if (BuildConfig.DEBUG) {
-            //打印日志
-            ARouter.openLog();
-            //开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！
-            //线上版本需要关闭,否则有安全风险)
-            ARouter.openDebug();
-        }
+//        if (BuildConfig.DEBUG) {
+//            //打印日志
+//            ARouter.openLog();
+//            //开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！
+//            //线上版本需要关闭,否则有安全风险)
+//            ARouter.openDebug();
+//        }
         ARouter.init(myApplication);
 
     }
