ab9e41afb2f6a25bcb7d37888cfcc825308182c1
GL <gl@GaoLeideMacBook-Pro.local>
2019-04-26 00:23:39 +0800

    实现全部和单独编译

--- Modularization/base/src/main/java/com/example/base/BaseApplication.java
+++ Modularization/base/src/main/java/com/example/base/BaseApplication.java
@@ -1,6 +1,7 @@
 package com.example.base;
 
 import android.app.Application;
+import android.support.multidex.MultiDex;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 
@@ -10,18 +11,18 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         initRouter(this);
-//        MultiDex.install(this);
+        MultiDex.install(this);
     }
 
     private void initRouter(BaseApplication myApplication) {
         // 这两行必须写在init之前，否则这些配置在init过程中将无效
-//        if (BuildConfig.DEBUG) {
-//            //打印日志
-//            ARouter.openLog();
-//            //开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！
-//            //线上版本需要关闭,否则有安全风险)
-//            ARouter.openDebug();
-//        }
+        if (BuildConfig.DEBUG) {
+            //打印日志
+            ARouter.openLog();
+            //开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！
+            //线上版本需要关闭,否则有安全风险)
+            ARouter.openDebug();
+        }
         ARouter.init(myApplication);
 
     }
